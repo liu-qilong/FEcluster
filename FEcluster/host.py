f457303551a5357fe3a236368b7431faf2b0137e
KNPOB <qilong-kirov.liu@outlook.com>
2023-06-26 12:18:29 +0800

    replace handcraft scp command with paramiko scp session

--- FEcluster/host.py
+++ FEcluster/host.py
@@ -4,18 +4,18 @@ import paramiko
 import subprocess
 
 class HostSession:
-    def __init__(self, addr: str, user: str, local_cwd: str, remote_cwd: str, session_name: str = None):
+    def __init__(self, addr: str, user: str, pwd: str, local_cwd: str, remote_cwd: str, session_name: str = None):
         self.session_name = session_name
         self.addr = addr
         self.user = user
+        self.pwd = pwd
         self.local_cwd = local_cwd
         self.remote_cwd = remote_cwd
 
         # init ssh connection
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh.load_system_host_keys()
-        self.ssh.connect(hostname=self.addr, username=self.user)
+        self.ssh.connect(hostname=self.addr, username=self.user, password=self.pwd)
 
         # create local_cwd and remote_cwd if they don't exist
         self.local_mkdir(self.local_cwd, cwd='C:\\')
@@ -81,27 +81,41 @@ class HostSession:
             self.local_shell_exec(
                 f'mkdir "{folder}"',
                 description = f'create local folder "{folder}"',
-                cwd=cwd,
+                cwd = cwd,
                 **kwargs,
                 )
 
-    def put_file(self, local_file_path: str, remote_file_folder: str, description: str = 'put file to remote folder', **kwargs):
+    def put_file(self, local_file_path: str, remote_file_folder: str, **kwargs):
         self.remote_mkdir(remote_file_folder)
-        file_name = os.path.split(local_file_path)[-1]
-        self.local_shell_exec(
-            f'scp "{local_file_path}" "{self.user}@{self.addr}:{os.path.join(self.remote_cwd, remote_file_folder, file_name)}"', 
-            description=description,
-            **kwargs,
-            )
+        description = f'put file "{local_file_path}" to remote folder "{remote_file_folder}"'
 
-    def get_file(self, remote_file_path: str, local_file_folder: str, description: str = 'get file from remote folder', **kwargs):
-        self.local_mkdir(local_file_folder)
-        file_name = os.path.split(remote_file_path)[-1]
-        self.local_shell_exec(
-            f'scp "{self.user}@{self.addr}:{os.path.join(self.remote_cwd, remote_file_path)}" "{os.path.join(local_file_folder, file_name)}"', 
-            description=description,
-            **kwargs,
+        try:
+            file_name = os.path.split(local_file_path)[-1]
+            scp = self.ssh.open_sftp()
+            scp.put(
+                os.path.join(self.local_cwd, local_file_path),
+                os.path.join(self.remote_cwd, remote_file_folder, file_name)
             )
+            self.write_log({'exit': 0}, description, **kwargs)
+
+        except:
+            self.write_log({'exit': 1}, description, **kwargs)
+
+    def get_file(self, remote_file_path: str, local_file_folder: str, **kwargs):
+        self.local_mkdir(local_file_folder)
+        description = f'get file "{remote_file_path}" to local folder "{local_file_folder}"'
+
+        try:
+            file_name = os.path.split(remote_file_path)[-1]
+            scp = self.ssh.open_sftp()
+            scp.get(
+                os.path.join(self.remote_cwd, remote_file_path),
+                os.path.join(self.local_cwd, local_file_folder, file_name)
+            )
+            self.write_log({'exit': 0}, description, **kwargs)
+
+        except:
+            self.write_log({'exit': 1}, description, **kwargs)
 
     def write_log(self, exec_return_dict: dict, description: str, print_stdout: bool = False, print_stderr: bool = False):
         current_time = time.strftime("%H:%M:%S")
