2a597044fa8cfeee65b91847710fb870fbee7b07
KNPOB <qilong-kirov.liu@outlook.com>
2023-08-01 21:10:39 +0800

    refine inheritance line of task

--- FEcluster/task.py
+++ FEcluster/task.py
@@ -1,13 +1,10 @@
 from FEcluster import mentat, host
 
 class Task:
-    def __init__(self, task_name: str = 'task', nps: int = 4, nts: int = 4, nte: int = 3, nsolver: int = 6):
+    def __init__(self, task_name: str = 'task'):
         self.task_name = task_name
         self.stop_watch = False
         self.complete = False
-
-        # parallel computation settings
-        self.nps, self.nts, self.nte, self.nsolver = nps, nts, nte, nsolver
 
     def mentat(self, mentat_obj: mentat.Mentat):
         pass
@@ -17,3 +14,10 @@ class Task:
 
     def watch(self, host_session: host.HostSession):
         pass
+
+
+class FETask(Task):
+    def __init__(self, nps: int = 4, nts: int = 4, nte: int = 3, nsolver: int = 6, **kwargs):
+        super().__init__(**kwargs)
+        # parallel computation settings
+        self.nps, self.nts, self.nte, self.nsolver = nps, nts, nte, nsolver
